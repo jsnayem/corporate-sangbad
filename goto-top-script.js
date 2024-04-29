10b40e0fe3a1447b1d91aefe31f92baf55741456
Md Nayem <113716708+jsnayem@users.noreply.github.com>
2024-04-29 17:32:15 +0600

    added more section

--- goto-top-script.js
+++ goto-top-script.js
@@ -1,5 +1,5 @@
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("gotoTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
